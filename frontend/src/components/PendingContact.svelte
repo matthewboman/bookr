f9ebbdf1167c4f12a59546726c291d9ce2d954f0
Matthew <matthew.boman@gmail.com>
2023-08-17 11:03:29 -0400

    frontend functionality for approve/delete contacts

---
+++ frontend/src/components/PendingContact.svelte
@@ -0,0 +1,78 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    import { ButtonGroup, Card, GradientButton} from 'flowbite-svelte'
+
+    export let contact
+
+    const dispatch = createEventDispatcher()
+
+    function approveContact() {
+        dispatch('approve', {
+            contact: contact
+        })
+    }
+
+    function deleteContact() {
+        dispatch('delete', {
+            contact: contact
+        })
+    }
+</script>
+
+<Card size="xl">
+    <div class="pending-contact">
+        <div class="pending-name">
+            { contact.displayName }
+        </div>
+        <div class="pending-address">
+            { contact.address }
+        </div>
+        <div class="pending-city">
+            { contact.city }
+        </div>
+        <div class="pending-state">
+            { contact.state }
+        </div>
+        <div class="pending-zip">
+            { contact.zipCode }
+        </div>
+        <div class="pending-buttons">
+            <ButtonGroup>
+                <GradientButton shadow="green" color="green" on:click={approveContact}>Approve</GradientButton>
+                <GradientButton shadow="red" color="red" on:click={deleteContact}>Delete</GradientButton>
+            </ButtonGroup>
+        </div>
+    </div>
+</Card>
+
+
+<style>
+    .pending-contact {
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .pending-name {
+
+    }
+
+    .pending-address {
+
+    }
+
+    .pending-city {
+
+    }
+
+    .pending-state {
+
+    }
+
+    .pending-zip {
+
+    }
+
+    .pending-buttons {
+
+    }
+</style>
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,3 +1,4 @@
 import { writable } from 'svelte/store'
 
+export const admin         = writable(false)
 export const authenticated = writable(false)
